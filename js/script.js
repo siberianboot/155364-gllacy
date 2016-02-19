c414ba79a46ccea47d82d2fbb3665af3db6cf688
siberianboot <va.realt@gmail.com>
2016-02-19 12:37:35 +0600

    Глэйси новые правки

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var catalogButton = document.querySelector('.main_navigation_catalog');
-var catalogPopup = document.querySelector('.catalog_menu');
+var catalogPopup = document.querySelector('.sub_catalog_menu');
 catalogButton.addEventListener("click", function(event) {
   event.preventDefault();
-  catalogPopup.classList.add('js_show_selector');
+  catalogPopup.classList.toggle('js_show_selector');
 });
