e74bc880c5156085e7741d4187dd5ebac3d2661f
siberianboot <va.realt@gmail.com>
2016-02-20 17:12:40 +0600

    Новое Глэйси

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var catalogButton = document.querySelector('.main_navigation_catalog');
-var catalogPopup = document.querySelector('.sub_catalog_menu');
-catalogButton.addEventListener("click", function(event) {
-  event.preventDefault();
-  catalogPopup.classList.toggle('js_show_selector');
-});
